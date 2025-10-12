b0f898be7e1ac5cec6f0446fc9d2e8a74ec5faee
rej <rzioma@acm.org>
2025-10-12 23:49:45 +0200

    Attempting to explain to Yosys that no FSM is needed when writing to d1reg_addr register.

--- src/IKAOPLL_reg.v
+++ src/IKAOPLL_reg.v
@@ -134,7 +134,10 @@ endgenerate
 
 //latch D1REG address, the original chip latches "decoded" register select bits
 reg     [7:0]   d1reg_addr;
-always @(posedge emuclk) if(!phi1ncen_n) if(addrreg_wrrq) d1reg_addr <= dbus_inlatch;
+wire            d1reg_wr_en = ~phi1ncen_n & addrreg_wrrq;
+// always @(posedge emuclk) if(!phi1ncen_n) if(addrreg_wrrq) d1reg_addr <= dbus_inlatch; // OLD
+always @(posedge emuclk) if(d1reg_wr_en) d1reg_addr <= dbus_inlatch; // REJ
+
 
 //D1REG pair, 0=modulator 1=carrier
 reg     [1:0]   am_reg, pm_reg, etyp_reg, ksr_reg;
