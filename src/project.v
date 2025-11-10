7855598a53bfcfb5feb258a849706e61529ae396
rej <rzioma@acm.org>
2025-11-10 12:09:54 +0200

    PWM in a separate file

--- src/project.v
+++ src/project.v
@@ -5,36 +5,6 @@
 
 `default_nettype none
 
-// A first-order sigma-delta modulator
-// It resembles a PWM, but actually is a PDM (Pulse Density Modulation)
-// https://en.wikipedia.org/wiki/Pulse-density_modulation
-// 
-// Implementaion based on https://www.fpga4fun.com/PWM_DAC_2.html
-
-module pwm #( parameter VALUE_BITS = 8 ) (
-    input  wire clk,
-    input  wire reset,
-
-    input  wire [VALUE_BITS-1:0]  value,
-
-    output wire out
-);
-    localparam ACCUMULATOR_BITS = VALUE_BITS + 1;
-    reg [ACCUMULATOR_BITS-1:0] accumulator;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            accumulator <= 0;
-        end else begin
-            // greater the value, the more often accumulator overflows
-            // every time the accumulator overflows, PDM outputs 1
-            accumulator <= accumulator[VALUE_BITS-1:0] + value;
-        end
-    end
-
-    assign out = accumulator[ACCUMULATOR_BITS-1]; // an overflow bit of the accumulator is the output of PDM
-endmodule
-
 module tt_um_rejunity_ym2413_ika_opll (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
